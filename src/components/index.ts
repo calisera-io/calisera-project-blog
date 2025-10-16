c18f6aa8c4ae72d9eb43d8efa8692b27b54fff71
hogh <hogh@lenscapades.com>
2025-10-16 22:08:14 +0200

    fix barrel export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,11 @@
-import { BlogHeader } from '@/components/blog-header';
-import { CoreFeaturesGrid } from '@/components/core-features-grid';
-import { FeaturedPostCard } from '@/components/featured-post-card';
-import { Footer } from '@/components/footer';
-import { GradientBackground } from '@/components/gradient-background';
-import { Header } from '@/components/header';
-import { RegularPostCard } from '@/components/regular-post-card';
-import { TagsFilter } from '@/components/tags-filter';
+import { BlogHeader } from './blog-header';
+import { CoreFeaturesGrid } from './core-features-grid';
+import { FeaturedPostCard } from './featured-post-card';
+import { Footer } from './footer';
+import { GradientBackground } from './gradient-background';
+import { Header } from './header';
+import { RegularPostCard } from './regular-post-card';
+import { TagsFilter } from './tags-filter';
 
 export {
 	BlogHeader,
